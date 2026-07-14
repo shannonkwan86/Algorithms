a95874bb42d50bc8fd3a9ed7db2681af20514947
shannonkwan86 <3406702953@qq.com>
2026-07-14 18:03:40 +0800

    新增 P3435 [POI 2006] OKR-Periods of Words 的代码实现

--- Algorithm/DS/P_3435_POI_2006_OKR_Periods_of_Words.cpp
+++ Algorithm/DS/P_3435_POI_2006_OKR_Periods_of_Words.cpp
@@ -4,7 +4,46 @@ using namespace std;
 
 void solve()
 {
-    
+    int k;
+    cin >> k;
+    string s;
+    cin >> s;
+    vector<int> pi(k);
+    vector<int> spi(k);
+    for (int i = 1; i < k; i++)
+    {
+        int j = pi[i - 1];
+        while (j > 0 && s[j] != s[i])
+        {
+            j = pi[j - 1];
+        }
+        if (s[j] == s[i])
+        {
+            j++;
+        }
+        pi[i] = j;
+    }
+    int ans = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if (pi[i] == 0)
+        {
+            spi[i] = 0;
+        }
+        else if (spi[pi[i] - 1] != 0)
+        {
+            spi[i] = spi[pi[i] - 1];
+        }
+        else
+        {
+            spi[i] = pi[i];
+        }
+        if (spi[i] != 0)
+        {
+            ans += (i + 1) - spi[i];
+        }
+    }
+    cout << ans << endl;
 }
 
 signed main()
