1e0af6153ad03afafd9fbafb38c1e9e27af0ce74
shannonkwan86 <3406702953@qq.com>
2026-07-16 15:24:16 +0800

    新增 P4824 [USACO15FEB] Censoring S 的代码实现，优化 KMP 算法并处理状态恢复；新增 .gitignore 文件以排除本地学习工作区

--- Algorithm/DS/P_4824_USACO_15_FEB_Censoring_S.cpp
+++ Algorithm/DS/P_4824_USACO_15_FEB_Censoring_S.cpp
@@ -4,14 +4,75 @@ using namespace std;
 
 void solve()
 {
-    
+    string s;
+    string t;
+    cin >> s >> t;
+
+    // 预处理t,找它的pi；
+    // 预处理模式串 t 的前缀函数
+    int m = t.size();
+    vector<int> pi(m);
+    for (int i = 1, j = 0; i < m; i++)
+    {
+        while (j && t[i] != t[j])
+        {
+            j = pi[j - 1];
+        }
+
+        if (t[i] == t[j])
+        {
+            j++;
+        }
+
+        pi[i] = j;
+    }
+
+    string ans;
+    vector<int> state;
+    int len = s.size();
+    int cnt = 0;
+    for (int i = 0; i < len; i++)
+    {
+        // ans += s[i];
+
+        char b = s[i];
+        while (cnt && t[cnt] != b)
+        {
+            cnt = pi[cnt - 1];
+        }
+        if (t[cnt] == b)
+        {
+            cnt++;
+        }
+        ans.push_back(b);
+        state.push_back(cnt);
+        if (cnt == m)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                ans.pop_back();
+                state.pop_back();
+            }
+
+            // 恢复删除后的 KMP 状态
+            if (state.empty())
+            {
+                cnt = 0;
+            }
+            else
+            {
+                cnt = state.back();
+            }
+        }
+    }
+    cout << ans << endl;
 }
 
 signed main()
 {
     ios::sync_with_stdio(false), cin.tie(0);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
     {
         solve();
