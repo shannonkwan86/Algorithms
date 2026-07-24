2ea15f7753755a8f5107169fb20133800339edb0
shannonkwan86 <3406702953@qq.com>
2026-07-24 21:45:20 +0800

    优化 NowCoder 题目 "Bitmask" 的代码实现，重构了位运算逻辑，更新了主程序逻辑

--- MultipleSchools/Nowcoder3/Bitmask.cpp
+++ MultipleSchools/Nowcoder3/Bitmask.cpp
@@ -2,55 +2,82 @@
 using namespace std;
 #define int long long
 
-int f(int x)
-{
-    int cnt = 0;
-    bool fg = 0;
-
-    while (x)
-    {
-        if (x % 2 == 1)
-        {
-            fg = 1;
-        }
-        else if (fg == 1)
-        {
-            cnt++;
-            fg = 0;
-        }
-        x /= 2;
-    }
-    if (fg == 1)
-    {
-        cnt++;
-        fg = 0;
-    }
-    return cnt;
-}
-
 void solve()
 {
     int n;
     cin >> n;
     vector<int> a(n + 1);
-    int ans = 0;
+    // int ans = 0;
+
+    vector<array<array<int, 2>, 2>> b(31);
 
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
-        ans += f(a[i]);
-    }
-    cout << ans << endl;
+        // ans += f(a[i]);
+        for (int j = 29; j >= 0; j--)
+        {
 
-    
+            int cur = (a[i] >> j) & 1;
+            int nxt = (a[i] >> (j + 1)) & 1;
+            b[j + 1][nxt][cur]++;
+        }
+    }
+
+    // cout << ans << endl;
+
     int m;
     cin >> m;
     for (int i = 1; i <= m; i++)
     {
         int type, x;
         cin >> type >> x;
+        for (int j = 1; j <= 30; j++)
+        {
+            array<array<int, 2>, 2> tmp{};
+            int high = (x >> j) & 1;
+            int low = (x >> (j - 1)) & 1;
+
+            if (type == 1)
+            {
+                for (int p = 0; p <= 1; p++)
+                {
+                    for (int q = 0; q <= 1; q++)
+                    {
+                        tmp[p & high][q & low] += b[j][p][q];
+                    }
+                }
+            }
+            if (type == 2)
+            {
+                for (int p = 0; p <= 1; p++)
+                {
+                    for (int q = 0; q <= 1; q++)
+                    {
+                        tmp[p | high][q | low] += b[j][p][q];
+                    }
+                }
+            }
+            if (type == 3)
+            {
+                for (int p = 0; p <= 1; p++)
+                {
+                    for (int q = 0; q <= 1; q++)
+                    {
+                        tmp[p ^ high][q ^ low] += b[j][p][q];
+                    }
+                }
+            }
+
+            b[j] = tmp;
+        }
+        int ans = 0;
+        for (int j = 1; j <= 30; j++)
+        {
+            ans += b[j][0][1];
+        }
+        cout << ans << endl;
     }
-    
 }
 
 signed main()
